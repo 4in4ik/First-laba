83942ff3e8fc6c647b1e55be49d4575eb3cce3ea
4in4ik <polynskijanton@gmail.com>
2023-03-13 13:47:36 +0300

    Second

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shoes O = new Shoes();
+            
+
+           
+
+            O.S(Console.ReadLine(),Convert.ToSingle(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            O.Print();
+            Console.ReadKey();
+        }
+    }
+    public class Shoes
+    {
+        string proizvoditel;
+        float size;
+        double price;
+        public void Print()
+        {
+            Console.WriteLine($"Производитель:{proizvoditel}\nРазмер:{size}\nЦена:{price}");       
+        }
+        public void S(string Sp, float Ss, double SP)
+        {
+            proizvoditel = Sp;
+            size = Ss;
+            price = SP;
+        }
+    }
+}
